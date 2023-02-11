87fef86aa2e9540542c3e2684826c80099e36376
xli1 <11111111>
2023-02-11 14:48:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,87 @@
+<script setup>
+// import HomeNav from './components/home/HomeNav.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+</script>
+
+<template>
+	<div container>
+		<header class="row">
+			<!-- <HomeNav></HomeNav> -->
+			<div class="col">
+				<span>JCL&YMJ</span>
+			</div>
+			<div class="navUl col">
+				<ul class="ul-list col-6 list-inline">
+					<li class="list-inline-item"><a href="#">首页</a></li>
+					<li class="list-inline-item"><a href="#">博客文档</a></li>
+					<li class="list-inline-item"><a href="#">JCL&YMJ</a></li>
+					<li class="list-inline-item"><a href="#">关于我</a></li>
+					<li class="list-inline-item"><a href="#">相册</a></li>
+					<li class="list-inline-item"><a href="#">时间线</a></li>
+				</ul>
+			</div>
+		</header>
+
+		<main>
+
+		</main>
+	</div>
+
+</template>
+<script>
+var _this;
+export default {
+	comments: {},
+	data() {
+		return {
+
+		}
+	},
+	onLoad(e) {
+
+	},
+	onShow() {
+
+	},
+
+	methods: {
+
+	}
+}
+</script>
+<style>
+/* 设置列表横排样式 */
+header ul {
+	display: inline-block;
+}
+
+/* 设置导航菜单靠右显示 */
+.navUl {
+	text-align: right;
+}
+
+/* 设置上方导航鼠标移上样式 */
+header ul li a {
+	text-decoration: none;
+	color: aquamarine;
+	
+}
+header ul li a:hover{
+	animation-name: navalink;
+	animation-duration: 1.5s;
+}
+
+/* 鼠标移上动画效果 */
+@keyframes navalink {
+	from {
+		color: aquamarine;
+	}
+	50%{
+		color: blue;
+	}
+	to {
+		color: red;
+	}
+}
+</style>
---
+++ src/components/home/HomeNav.vue
@@ -0,0 +1,31 @@
+
+
+<script setup>
+defineProps({
+
+})
+</script>
+
+<template>
+  <div class="d-inline-flex p-">
+    <div class="p-6" style="display: inline;">
+      <span>JCL&YMJ</span>
+    </div>
+    <div class="p-2" style="display: inline;">
+      <ul class="ul-list col-6 list-inline">
+        <li class="list-inline-item">首页</li>
+        <li class="list-inline-item">博客文档</li>
+        <li class="list-inline-item">JCL&YMJ</li>
+        <li class="list-inline-item">关于我</li>
+        <li class="list-inline-item">相册</li>
+        <li class="list-inline-item">时间线</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style>
+.nav-div div {
+  display: inline-block;
+}
+</style>
